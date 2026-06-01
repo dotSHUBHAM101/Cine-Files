fcf42c321a48a2163cfdabe0c42017a418c4d408
SHUBHAM MISHRA <shubhammcse@gmail.com>
2026-06-01 14:07:08 +0530

    Feat: added a small feature

--- src/App.jsx
+++ src/App.jsx
@@ -43,6 +43,7 @@ function App() {
                     <NavLink to = '/trending'>Trending</NavLink>
                     <NavLink to = '/Movies'>Movies</NavLink>
                     <NavLink to = '/Tvseries'>Tv series</NavLink>
+                    <NavLink to = '/search'>Search 🔍</NavLink>
                     </div>
 
 
